010fa64fd814f38562903f2c1d17f3c2ec138f91
Balázs Pete <bazsip@gmail.com>
2012-10-16 03:24:03 +0100

    Removing commented pseudo code from Algorithms.rb

--- Assignment 1/Algorithms.rb
+++ Assignment 1/Algorithms.rb
@@ -1,16 +1,5 @@
 INFINITY = 1.0/0
 
-# αβ (node Node, int Ht, real Achievable, real Hope) 
-#  if Ht=0 or no moves exist
-#   then Return Evaluation (Node)
-#  else {real Temp;
-#        for each move M at node Node,
-#         {generate NewNode from Node & M;
-# Temp:= - αβ(Newnode, Ht-1, -Hope, -Achievable); Destroy NewNode;
-# If Temp>=Hope then Return Temp; Achievable:=Max(Temp, Achievable) }}
-# ￼Return Achievable }
-
-
 def negamax_alpha_beta node, height, achievable=-INFINITY, hope=INFINITY, count=0
 	_return = lambda {|r| [r, count +1]}
 
