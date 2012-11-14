c1caa81eccf82691a2cac50000292a01fb2b0dd4
Balázs Pete <bazsip@gmail.com>
2012-11-14 08:39:32 +0000

    Adding remaining of Game

--- Yavalath/Yavalath/Game.cs
+++ Yavalath/Yavalath/Game.cs
@@ -1,15 +1,32 @@
 using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Yavalath
 {
 	public class Game
 	{
 		private Board GameBoard;
+        public Player CurrentPlayer { get; private set; }
 
-		public Game ()
+        private IDictionary<Player, int> Players;
+
+		public Game (Player player1, Player player2)
 		{
 			GameBoard = new Board();
+            Players = new Dictionary<Player, int>(){
+                {player1, -1},
+                {player2, 1}
+            };
+
+            CurrentPlayer = player1;
 		}
+
+        private void NextPlayer() 
+        {
+
+        }
 	}
 }
 
