aa26fc740bbdde02a5aecf557b633a30cafb47cc
Balázs Pete <bazsip@gmail.com>
2012-11-19 00:58:58 +0000

    Adding Algorithms class

---
+++ Yavalath/Yavalath/Algorithms.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Yavalath
+{
+    public class Algorithms
+    {
+        public Algorithms()
+        {
+        }
+
+        public static int Evaluation(Board gameBoard, int player)
+        {
+            /*
+             *Empty-piece-piece-empty (which is bad)
+Empty-piece-empty-piece-empty (also bad)
+Empty-piece-piece-empty-piece (good)
+(empty-piece-emty-empty-piece (good)
+             *
+             *
+             */
+
+
+
+            return 0;
+        }
+    }
+}
+
